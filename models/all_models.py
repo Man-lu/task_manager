32dfec5de5c387bf189884e4d826a5c8160effc0
Man-lu <lucky.manaswe@gmail.com>
2021-02-20 13:54:46 +0200

    added server configs

--- models/all_models.py
+++ models/all_models.py
@@ -5,7 +5,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mydb.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///mydb.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
