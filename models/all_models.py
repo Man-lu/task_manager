5136b19d0ecf6083e74d87ef8883a11da58e43ac
Man-lu <lucky.manaswe@gmail.com>
2021-02-26 11:31:50 +0200

    added authentication handler

--- models/all_models.py
+++ models/all_models.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///mydb.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = os.environ.get("WarenaManaswe")
+# app.secret_key = os.environ.get("WarenaManaswe")
 db = SQLAlchemy(app)
 
 
